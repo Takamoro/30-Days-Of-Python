44615e6f23902b1c13d07679255af0919f0799f0
Takamoro <112599022+Takamoro@users.noreply.github.com>
2022-09-02 22:17:37 -0500

    Create variable.py

---
+++ 02_Day_Variables_builtin_functions/variable.py
@@ -0,0 +1,14 @@
+#Day2:30 days of python programming
+first_name = 'Takamoro'
+last_name = 'Reap'
+full_name = first_name, last_name
+country = 'Perú'
+city = 'Mollendo'
+age = 33
+year = 2022
+is_married = False
+is_true = False
+is_light_on = True
+first_name, last_name, country, age, is_married = 'Jhonatan', 'Reyes', 'Perú', 33, False
+print(type(first_name))
+
